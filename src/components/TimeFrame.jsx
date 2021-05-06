9ec600725961e6c9dbcdd76be672a4a8d0bec05c
Joao Vanelli <vanellijoao@gmail.com>
2021-05-06 10:00:58 -0300

    Adicionando parte do esqueleto para checagem

--- src/components/TimeFrame.jsx
+++ src/components/TimeFrame.jsx
@@ -4,7 +4,34 @@ class TimeFrame extends React.Component {
   render() {
     return (
       <div className="timeframe">
-        <div className="date"></div>
+        <div className="date">
+          <div className="month">
+            <p>month</p>
+            <div></div>
+          </div>
+          <div className="day">
+            <p>day</p>
+            <div></div>
+          </div>
+          <div className="am-pm">
+            <p>am</p>
+            <input type="radio" name="" id=""/>
+            <p>pm</p>
+            <input type="radio" name="" id=""/>
+          </div>
+          <div className="hour">
+            <p>hour</p>
+            <div></div>
+          </div>
+          <div className="min">
+            <p>min</p>
+            <div></div>
+          </div>
+          <div className="sec">
+            <p>sec</p>
+            <div></div>
+          </div>
+        </div>
         <div className="legend">
           <p>{ this.props.legend }</p>
         </div>
